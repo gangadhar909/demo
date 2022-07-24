a1e6c35b8c02aa9b2cbc51e8b6e82c7b2842730e
gangadhar909 <gangadhar9392@gmail.com>
2022-07-24 20:33:44 +0530

    added java file

---
+++ variablesUpdate.java
@@ -0,0 +1,11 @@
+public class variablesUpdate {
+    public static void main(String[] args)
+    {
+        int numberList[] = {108,467,890,60,50};
+		for(int i = 0; i < numberList.length; i++){
+			if(numberList[i]<350){
+				System.out.println(numberList[i]);
+    }
+}
+    }
+}
